01f27625ad817503842274df1c60873a6d54d1b1
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-02-29 17:45:43 +0100

    Completed Lecture 7

--- src/test/java/lectures/Lecture7.java
+++ src/test/java/lectures/Lecture7.java
@@ -1,65 +1,90 @@
 package lectures;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 import beans.Car;
-import com.google.common.collect.ImmutableList;
-import java.math.BigDecimal;
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
-import java.util.stream.Collectors;
+import beans.Person;
 import mockdata.MockData;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
 public class Lecture7 {
 
-  @Test
-  public void count() throws Exception {
+    @Test
+    public void count() throws Exception {
+        List<Person> people = MockData.getPeople();
 
-  }
+        long count = people.stream()
+                .filter(person -> person.getGender().equalsIgnoreCase("female"))
+                .count();
 
-  @Test
-  public void min() throws Exception {
+        System.out.println("Number of women " + count);
+    }
 
-  }
+    @Test
+    public void min() throws Exception {
+        List<Car> cars = MockData.getCars();
 
-  @Test
-  public void max() throws Exception {
+        double min = cars.stream()
+                .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+                .mapToDouble(car -> car.getPrice())
+                .min()
+                .getAsDouble();
 
-  }
+        System.out.println("Minimum car price " + min);
+    }
 
+    @Test
+    public void max() throws Exception {
+        List<Car> cars = MockData.getCars();
 
-  @Test
-  public void average() throws Exception {
-    List<Car> cars = MockData.getCars();
+        double max = cars.stream()
+                .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+                .mapToDouble(car -> car.getPrice())
+                .max()
+                .getAsDouble();
 
-  }
+        System.out.println("Maximum car price " + max);
+    }
 
-  @Test
-  public void sum() throws Exception {
-    List<Car> cars = MockData.getCars();
-    double sum = cars.stream()
-        .mapToDouble(Car::getPrice)
-        .sum();
-    BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
-    System.out.println(sum);
-    System.out.println(bigDecimalSum);
+    @Test
+    public void average() throws Exception {
+        List<Car> cars = MockData.getCars();
 
-  }
+        double average = cars.stream()
+                .mapToDouble(car -> car.getPrice())
+                .average()
+                .orElse(0);
 
-  @Test
-  public void statistics() throws Exception {
-    List<Car> cars = MockData.getCars();
-    DoubleSummaryStatistics statistics = cars.stream()
-        .mapToDouble(Car::getPrice)
-        .summaryStatistics();
-    System.out.println(statistics);
-    System.out.println(statistics.getAverage());
-    System.out.println(statistics.getCount());
-    System.out.println(statistics.getMax());
-    System.out.println(statistics.getMin());
-    System.out.println(statistics.getSum());
-  }
+        System.out.println("Average car price " + average);
+    }
 
+    @Test
+    public void sum() throws Exception {
+        List<Car> cars = MockData.getCars();
+
+        double sum = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .sum();
+        BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
+
+        System.out.println("Sum of all car prices " + bigDecimalSum);
+    }
+
+    @Test
+    public void statistics() throws Exception {
+        List<Car> cars = MockData.getCars();
+
+        DoubleSummaryStatistics statistics = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .summaryStatistics();
+
+        System.out.println("Cars statistics: " + statistics);
+        System.out.println(statistics.getAverage());
+        System.out.println(statistics.getCount());
+        System.out.println(statistics.getMax());
+        System.out.println(statistics.getMin());
+        System.out.println(statistics.getSum());
+    }
 }
