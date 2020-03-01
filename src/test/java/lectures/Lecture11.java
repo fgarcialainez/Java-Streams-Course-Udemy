a7df3b80694841a8ebe35dfcd00ab92637140b44
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-03-01 10:01:30 +0100

    Completed Lecture 11

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -1,22 +1,36 @@
 package lectures;
 
 import com.google.common.collect.ImmutableList;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture11 {
 
-  @Test
-  public void joiningStrings() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    @Test
+    public void joiningStrings() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
 
-  }
+        String join = "";
 
-  @Test
-  public void joiningStringsWithStream() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+        for (String name : names) {
+            join += name + ", ";
+        }
 
-  }
+        System.out.println(join.substring(0, join.length() - 2));
+    }
+
+    @Test
+    public void joiningStringsWithStream() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+
+        String join = names.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.joining("|"));
+
+        System.out.println(join);
+    }
 }
